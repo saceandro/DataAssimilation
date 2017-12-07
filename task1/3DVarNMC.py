fixed variance and implemented EnKF

task1/3DVarNMC.py
@@ -224,7 +224,7 @@ obs_index = np.random.choice(N, M, replace=False)
 
 true_orbit = np.loadtxt("data/year.1.dat")
 
-y = np.loadtxt("data/observed6h.1.dat")
+y = np.loadtxt("data/observed." + str(it) + ".1.dat")
 
 R = np.zeros((M, M))
 np.fill_diagonal(R, 1)
@@ -325,6 +325,16 @@ plt.legend()
 plt.show()
 
 print ("RMSE: ", np.mean([np.linalg.norm(xa[i] - true_orbit[i*it])/math.sqrt(N) for i in range(1000,(int(len(t)/it)))]))
+
+#%%
+plt.plot(t_day_every6h, [np.linalg.norm(xa[i] - true_orbit[i*it])/math.sqrt(N) for i in range(int(len(t)/it))], label='x assimilation norm')
+plt.plot(t_day_every6h, [np.linalg.norm(y[i] - true_orbit[i*it])/math.sqrt(N) for i in range(int(len(t)/it))], label='x observation norm')
+plt.plot(t_day_every6h, [np.linalg.norm(Pa[i])/math.sqrt(N) for i in range(int(len(t)/it))], label='P norm')
+plt.legend()
+plt.show()
+
+print ("assimilation RMSE: ", np.mean([np.linalg.norm(xa[i] - true_orbit[i*it])/math.sqrt(N) for i in range(1000,(int(len(t)/it)))]))
+print ("observation RMSE: ", np.mean([np.linalg.norm(y[i] - true_orbit[i*it])/math.sqrt(N) for i in range(1000,(int(len(t)/it)))]))
 
 
 ##%%

task1/3_open_interval.py
@@ -71,7 +71,8 @@ class RK4:
         steps = int(T/self.dt)
         o = self.orbit(gradient, t0, x0, T)
         for i in range(steps):
-            o[i] += stddev * rng.randn()
+            for j in range(self.N):                
+                o[i][j] += stddev * rng.randn()
         return o
 
     def true_observed(self, gradient, t0, x0, T, stddev, rng):
@@ -79,7 +80,8 @@ class RK4:
         o = self.orbit(gradient, t0, x0, T)
         obs = np.copy(o)
         for i in range(steps):
-            obs[i] += stddev * rng.randn()
+            for j in range(self.N):
+                obs[i][j] += stddev * rng.randn()
         return o, obs
 
 def plot_orbit(dat):
@@ -95,13 +97,13 @@ def plot_orbit(dat):
 #%%
 N = 40
 F = 8
-stddev = 1
+stddev = 1.
 year = 2
 day = 365 * year
 dt = 0.01
 T = day * 0.2
 steps = int(T/dt)
-interval = 10
+interval = 5
 
 lorenz = Lorenz96(N, F)
 rk4 = RK4(N, dt)
@@ -118,6 +120,8 @@ for seed in seeds:
     x0[rng.randint(N)] += 0.1*rng.randn()
     
     true_orbit, observed = rk4.true_observed(lorenz.gradient, 0., x0, T, stddev, rng)
+    
+    print ("observation RMSE: ", np.mean([np.linalg.norm(observed[i] - true_orbit[i])/math.sqrt(N) for i in range(steps)]))
     
     assimilation_xzero = observed[rng.randint(len(observed))]
     with open('data/assimilation_xzero.' + str(seed) + '.dat', 'w') as ff:

task1/KF6norm_obs.py
@@ -188,14 +188,14 @@ it = 5
 minute_steps = int(T/dt)
 steps = int(minute_steps/it)
 stddev = 1
-M = 30
+M = 40
 
 obs_index = np.random.choice(N, M, replace=False)
 
 
 true_orbit = np.loadtxt("data/year.1.dat")
 
-y = np.loadtxt("data/observed6h.1.dat")
+y = np.loadtxt("data/observed." + str(it) + ".1.dat")
 
 R = np.zeros((M, M))
 np.fill_diagonal(R, 1)
@@ -223,7 +223,7 @@ xf[0] = np.loadtxt("data/assimilation_xzero.2.dat")
 xa = np.zeros((steps, N))
 
 Pf = np.zeros((minute_steps, N, N))
-Pf[0] = np.loadtxt("data/cov.1.dat")
+Pf[0] = np.loadtxt("data/cov." + str(it) + ".1.dat")
 
 Pa = np.zeros((steps, N, N))
 
@@ -255,12 +255,21 @@ for j in range(1):
 
 #%%
 plt.plot(t_day_every6h, [np.linalg.norm(xa[i] - true_orbit[i*it])/math.sqrt(N) for i in range(int(len(t)/it))], label='x norm')
-plt.plot(t_day_every6h, [np.linalg.norm(Pa[i])/math.sqrt(N) for i in range(int(len(t)/it))], label='P norm')
+plt.plot(t_day_every6h, [np.linalg.norm(Pa[i].diagonal())/math.sqrt(N) for i in range(int(len(t)/it))], label='P norm')
 plt.legend()
 plt.show()
 
 print ("RMSE: ", np.mean([np.linalg.norm(xa[i] - true_orbit[i*it])/math.sqrt(N) for i in range(1000,(int(len(t)/it)))]))
 
+#%%
+plt.plot(t_day_every6h, [np.linalg.norm(xa[i] - true_orbit[i*it])/math.sqrt(N) for i in range(int(len(t)/it))], label='x assimilation norm')
+plt.plot(t_day_every6h, [np.linalg.norm(y[i] - true_orbit[i*it])/math.sqrt(N) for i in range(int(len(t)/it))], label='x observation norm')
+plt.plot(t_day_every6h, [np.linalg.norm(Pa[i])/math.sqrt(N) for i in range(int(len(t)/it))], label='P norm')
+plt.legend()
+plt.show()
+
+print ("assimilation RMSE: ", np.mean([np.linalg.norm(xa[i] - true_orbit[i*it])/math.sqrt(N) for i in range(1000,(int(len(t)/it)))]))
+print ("observation RMSE: ", np.mean([np.linalg.norm(y[i] - true_orbit[i*it])/math.sqrt(N) for i in range(1000,(int(len(t)/it)))]))
 
 ##%%
 #plt.plot([i for i in range(7000,7051)], [np.linalg.norm(x[i] - true_orbit[i])/N for i in range(7000,7051)], label='x norm')